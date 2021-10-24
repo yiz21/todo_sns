248f8cb05690e9456cca854d4084e90ff5551779
yiz <yaiza0521@gmail.com>
2021-10-24 17:39:57 +0900

    refactoring

--- components/TodoList.jsx
+++ components/TodoList.jsx
@@ -47,68 +47,63 @@ export default function  TodoList() {
     }
   }
 
-  const updateLocalTodo = (id, text) => {
-
-  }
-
-
   return (
     <List component="nav" disablePadding dense>
-    {!todo.loading && (
-      todo.current.map((t) => {
-        const labelId = `checkbox-list-label-${t.id}`;
-        return (
-          <div key={t.id}>
-            {
-              (mode.current == 'normal' || mode.current == 'create') &&
-              <ListItem
-                key={t.id}
-                selected={t == selected}
-                disablePadding
-                className={classes.listItem}
-                secondaryAction={
-                  <ListMenu
-                    handleDelete={() => {todo.deleteTodo(t)}}
-                    handleShare={() => {todo.shareTodo(t)}}
-                  />
-                }
-              >
-                <ListItemButton
-                  className={classes.checkBox}
-                  onClick={() => {showTodoList(t); setSelected(t);}}
-                  divider={todo.current.slice(-1)[0] != t}
+      {!todo.loading && (
+        todo.current.map((t) => {
+          const labelId = `checkbox-list-label-${t.id}`;
+          return (
+            <div key={t.id}>
+              {
+                (mode.current == 'normal' || mode.current == 'create') &&
+                <ListItem
+                  key={t.id}
+                  selected={t == selected}
+                  disablePadding
+                  className={classes.listItem}
+                  secondaryAction={
+                    <ListMenu
+                      handleDelete={() => {todo.deleteTodo(t)}}
+                      handleShare={() => {todo.shareTodo(t)}}
+                    />
+                  }
                 >
-                  <ListItemText id={labelId} primary={t.name} />
-                </ListItemButton>
-              </ListItem>
-            }
-            {
-              mode.current == 'edit' &&
-              <ListItem
-                key={t.id}
-                selected={t == selected}
-                onClick={() => {setSelected(t);}}
-                disablePadding
-                className={classes.listItem}
-              >
-                <ListItemButton
-                  className={classes.checkBox}
-                  divider={todo.current.slice(-1)[0] != t}
+                  <ListItemButton
+                    className={classes.checkBox}
+                    onClick={() => {showTodoList(t); setSelected(t);}}
+                    divider={todo.current.slice(-1)[0] != t}
+                  >
+                    <ListItemText id={labelId} primary={t.name} />
+                  </ListItemButton>
+                </ListItem>
+              }
+              {
+                mode.current == 'edit' &&
+                <ListItem
+                  key={t.id}
+                  selected={t == selected}
+                  onClick={() => {setSelected(t);}}
+                  disablePadding
+                  className={classes.listItem}
                 >
-                  <input
-                    type="text"
-                    value={t.name}
-                    className={classes.cardInput}
-                    onChange={(e) => todo.updateLocalTodo({...t, name: e.target.value})}
-                    onBlur={() => todo.updateTodo(t)}
-                  />
-                </ListItemButton>
-              </ListItem>
-            }
-          </div>
-        ) 
-      })
-    )}
-  </List>
+                  <ListItemButton
+                    className={classes.checkBox}
+                    divider={todo.current.slice(-1)[0] != t}
+                  >
+                    <input
+                      type="text"
+                      value={t.name}
+                      className={classes.cardInput}
+                      onChange={(e) => todo.updateLocalTodo({...t, name: e.target.value})}
+                      onBlur={() => todo.updateTodo(t)}
+                    />
+                  </ListItemButton>
+                </ListItem>
+              }
+            </div>
+          ) 
+        })
+      )}
+    </List>
   );
 }
